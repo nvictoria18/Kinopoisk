346bdf5089b241559801f2f3ba98cd78bc90ebfd
nvictoria18 <v.nesterenko@nv.dunice.net>
2025-07-07 13:16:12 +0300

    develop: responsive detail

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -19,48 +19,50 @@ const Detail = ({
         backgroundImage: `url(${Background})`
     }} className="w-full relative z-0 bg-no-repeat bg-cover bg-[position:0_5%] pb-[156px]">
         <Navbar isPermission={false} />
-        <div className="relative w-full mx-auto max-w-[700px] md:max-w-[75rem] mt-10 md:px-0 px-5">
-            <div className="absolute  md:w-full h-full max-w-[660px] box-border md:mx-0 mx-5 md:max-w-[75rem] top-0 left-0 bg-linear-to-t from-secondary-700 rounded-2xl to-primary-700 opacity-40 md:rounded-[40px]" />
-            <img src={bg} className="h-[30rem] max-w-[700px] w-full md:max-w-[75rem] md:rounded-[40px] object-cover rounded-2xl" />
-            <div className="absolute left-2/4 w-3/4 md:w-full -translate-x-1/2 md:translate-0 md:left-20 top-102 bg-[#20283ECC] backdrop-blur-xl p-10 md:max-w-[35rem] rounded-3xl space-y-2">
-                <div className="text-primary-200 body-extra-small flex gap-2">
-                    <Link to="/">MaileHereko</Link> <span className="text-gray-400">/</span> <Link to="/movies">Movies</Link>
+        <div className="md:px-0 px-5">
+            <div className="relative w-full mx-auto max-w-[700px] md:max-w-[75rem] mt-10">
+                <div className="absolute w-full h-full box-border md:max-w-[75rem] top-0 left-0 bg-linear-to-t from-secondary-700 rounded-2xl to-primary-700 opacity-40 md:rounded-[40px]" />
+                <img src={bg} className="h-[30rem] max-w-[700px] w-full md:max-w-[75rem] md:rounded-[40px] object-cover rounded-2xl" />
+                <div className="absolute left-2/4 w-3/4 md:w-full -translate-x-1/2 md:translate-0 md:left-20 top-102 bg-[#20283ECC] backdrop-blur-xl p-10 md:max-w-[35rem] rounded-3xl space-y-2">
+                    <div className="text-primary-200 body-extra-small flex gap-2">
+                        <Link to="/">MaileHereko</Link> <span className="text-gray-400">/</span> <Link to="/movies">Movies</Link>
+                    </div>
+                    <div className="text-gray-50 heading-three text-2xl md:text-[2rem]">{name ?? 'Avengers: Endgame'}</div>
                 </div>
-                <div className="text-gray-50 heading-three text-[2rem]">{name ?? 'Avengers: Endgame'}</div>
             </div>
         </div>
-        <div className="w-full mx-auto max-w-[700px] md:max-w-[65rem] flex gap-20 mt-[9.5rem] flex-wrap items-center md:items-start md:flex-nowrap">
-            <div className="md:shrink-0 shrink-1">
+        <div className="md:px-0 px-5 w-full mx-auto max-w-[75rem] max-[700px]:flex-col max-[700px]:items-center md:max-w-[65rem] flex gap-10 md:gap-20 mt-[9.5rem] flex-row items-start md:flex-nowrap">
+            <div className="shrink-0 w-full max-w-[20rem] md:max-w-[30rem]">
                 <img className="rounded-3xl" src={Poster} />
             </div>
             <div
                 className="flex flex-col gap-6"
             >
-                <div className="heading-four text-gray-50">Part of the journey is the end.</div>
+                <div className="heading-four md:text-2xl text-xl text-gray-50">Part of the journey is the end.</div>
                 <div
-                    className="text-gray-300 body-large"
+                    className="text-gray-300 body-large text-base md:text-xl"
                 >
                     After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos' actions and restore order to the universe once and for all, no matter what consequences may be in store.
                 </div>
-                <div className="bg-black-65 flex gap-1 px-2 py-1 text-warning-500 body-regular text-base items-center w-[59px] rounded-lg">
+                <div className="bg-black-65 flex gap-1 px-2 py-1 text-warning-500 body-regular text-sm md:text-base items-center w-[59px] rounded-lg">
                     <Star />
                     8.3
                 </div>
                 <div className="space-y-2">
-                    <div className="text-gray-400 body-regular">Type</div>
-                    <div className="text-gray-100 body-large">Movie</div>
+                    <div className="text-gray-400 body-regular text-sm md:text-base">Type</div>
+                    <div className="text-gray-100 body-large text-base md:text-xl">Movie</div>
                 </div>
                 <div className="space-y-2">
-                    <div className="text-gray-400 body-regular">Release Date:</div>
-                    <div className="text-gray-100 body-large">2019-04-24</div>
+                    <div className="text-gray-400 body-regular text-sm md:text-base">Release Date:</div>
+                    <div className="text-gray-100 body-large text-base md:text-xl">2019-04-24</div>
                 </div>
                 <div className="space-y-2">
-                    <div className="text-gray-400 body-regular">Run time:</div>
-                    <div className="text-gray-100 body-large">181 min</div>
+                    <div className="text-gray-400 body-regular text-sm md:text-base">Run time</div>
+                    <div className="text-gray-100 body-large text-base md:text-xl">181 min</div>
                 </div>
                 <div className="space-y-2">
-                    <div className="text-gray-400 body-regular">Genres</div>
-                    <div className="text-gray-100 body-large flex gap-2">
+                    <div className="text-gray-400 body-regular text-sm md:text-base">Genres</div>
+                    <div className="text-gray-100 body-large text-base md:text-xl flex gap-2">
                         <span>Adventure,</span>
                         <span>Science Fiction,</span>
                         <span>Action</span>
